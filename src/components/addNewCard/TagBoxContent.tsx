3cb414dc4aa76458ea58de722a566a1c2879821f
HassanThePassionate <maqsoodia777@gmail.com>
2025-03-12 21:51:47 +0500

    Update

--- src/components/addNewCard/TagBoxContent.tsx
+++ src/components/addNewCard/TagBoxContent.tsx
@@ -5,24 +5,41 @@ import { toast } from "react-toastify";
 
 import { cn } from "@/lib/utils";
 import { useFormContext } from "@/context/from-Context";
+import { useBookmarks } from "@/context/BookmarkContext";
+import { Card } from "@/types/TabCardType";
 
 type Tags = {
   name: string;
   id: number;
 };
+
 interface Props {
   actionBtns?: boolean;
   className?: string;
   selectedCardTags?: Tags[];
 }
 const TagBoxContent = ({ actionBtns, className }: Props) => {
-  const { prevStep, resetForm, isLoading, setIsLoading } = useFormContext();
-
+  const { prevStep, resetForm, isLoading, setIsLoading, formData } =
+    useFormContext();
+  const { addCard } = useBookmarks();
   const handleSaveBtn = () => {
     setIsLoading(true);
+    const newCard: Card = {
+      id: Date.now(),
+      title: formData.title,
+      icon: `https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${formData.url}/&size=32`,
+      path: formData.url,
+      des: formData.description,
+      tags: formData.tags.map((tag) => ({
+        id: crypto.randomUUID(),
+        name: tag,
+      })),
+    };
+
+    addCard(newCard);
+    toast.success("Bookmark Added");
     setTimeout(() => {
       setIsLoading(false);
-      toast.success("Bookmark Added");
       resetForm();
     }, 2000);
   };
