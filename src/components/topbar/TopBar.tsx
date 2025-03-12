a7bba9476ff4cfaa6ecb2d1191062685b2c87dab
HassanThePassionate <maqsoodia777@gmail.com>
2025-03-13 02:36:50 +0500

    Update

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,12 +1,13 @@
-import { categoriesData } from "@/constant/categoriesData";
 import DropDown from "./DropDown";
 import TopNav from "./TopNav";
 import { usePageContext } from "@/context/PageContext";
 import { categories } from "@/constant/categories";
 import History from "../historyPage/History";
+import { useBookmarks } from "@/context/BookmarkContext";
 
 const TopBar = () => {
   const { page } = usePageContext();
+  const { categories: categoriesData } = useBookmarks();
   const isDownloadPage = page === "downloads";
   const categoryData = isDownloadPage ? categories : categoriesData;
   return (
